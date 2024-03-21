5ee03b44bf7272eec60ade859395dc1043573355
ggabl1 <gabigcastro2@gmail.com>
2024-03-21 10:27:07 -0300

    adicionando conteudo da fatec

---
+++ aulasdealgoritmopython/algoritmo.py
@@ -0,0 +1,9 @@
+km = float(input('escreva o quilometro: '))
+consumo = float(input('escreva o consumo total: '))
+resultado = km/consumo
+print ('o resultado eh: ', resultado)
+if resultado >= 1.8:
+    print('o consumo por litro é baixo')
+elif resultado < 1.8:
+    print('o consumo por litro é alto')
+    
